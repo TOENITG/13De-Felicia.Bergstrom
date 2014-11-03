dcc186c924510612d894e735dcb994276ffad33e
xFeber25 <weyenriah@gmail.com>
2014-11-03 14:42:50 +0100

    weeeell
    hello

--- Programmering/Inlamningsuppgift2/src/inlamningsuppgift2/InlamningsUppgift2v2.java
+++ Programmering/Inlamningsuppgift2/src/inlamningsuppgift2/InlamningsUppgift2v2.java
@@ -3,98 +3,70 @@ import java.util.Scanner;
 
 public class InlamningsUppgift2v2 {
 
+    //Error messages
+    static double felsteg() {
+        //User input is OKIDOKI to this method
+        Scanner user_input=new Scanner( System.in );
+
+        try {
+        String something=user_input.next();
+        return Double.parseDouble(something);
+        }
+        catch (NumberFormatException err) {
+            System.out.println( "Detta är inte ett tal, försök igen!" );
+        }
+        return
+    }
+    
     public static void main(String[] args) {
         
-        Scanner user_input=new Scanner( System.in );
-        
-        //The answer
+
         double answer;
         
-        //Someone can now write in the consol, first number
-        String something;
-        System.out.println("Första talet, tack!");
-        something=user_input.next();
         
-        //Converting String to Int, first number
-        String first_number=something;
-        double firstnumber = Double.parseDouble(first_number);
+        //Someone can now write in the consol, first number
+        System.out.print("Första talet, tack! ");
+        double firstnumber = felsteg();
+        
         
         //Second number
-        String somethingelse;
-        System.out.println("Andra talet, tack!");
-        somethingelse=user_input.next();
+        System.out.println("Andra talet, tack! ");
+        double secondnumber = felsteg();
         
-        //Converting second number
-        String second_number=somethingelse;
-        double secondnumber = Double.parseDouble(second_number);
-        
-        //Doing the math
+        //Doing the math with second number
         answer = firstnumber + secondnumber;
         
         System.out.println("Addition: " + answer);
         
+        
         //Third number
-        String somethingelseagain;
-        System.out.println("Tredje talet, tack!");
-        somethingelseagain=user_input.next();
+        System.out.println("Tredje talet, tack! ");
+        double thirdnumber = felsteg();
         
-        //Converting third number
-        String third_number=somethingelseagain;
-        double thirdnumber = Double.parseDouble(third_number);
-        
-        //Doing the math
+        //Doing the math with third number
         answer = (firstnumber + secondnumber) - thirdnumber;
         
         System.out.println("Subtraktion: " + answer);
         
+        
         //Fourth number
-        String somethingelseelse;
-        System.out.println("Fjärde talet, tack!");
-        somethingelseelse=user_input.next();
+        System.out.println("Fjärde talet, tack! ");
+        double fourthnumber = felsteg();
         
-        //Converting fourth number
-        String fourth_number=somethingelseelse;
-        double fourthnumber = Double.parseDouble(fourth_number);
-        
-        //Doing the math
+        //Doing the math with fourth number
         answer = ((firstnumber + secondnumber) - thirdnumber) * fourthnumber;
         
         System.out.println("Multiplikation: " + answer);
         
+        
         //Fifth number
-        String somethingagainagain;
-        System.out.println("Femte talet, tack!");
-        somethingagainagain=user_input.next();
+        System.out.println("Femte talet, tack! ");
+        double fifthnumber = felsteg();
         
-        //Converting fifth number
-        String fifth_number=somethingagainagain;
-        double fifthnumber = Double.parseDouble(fifth_number);
-        
-        //Doing the math
+        //Doing the math with fifth number
         answer =(((firstnumber + secondnumber) - thirdnumber) * fourthnumber / fifthnumber);
         
         System.out.println("Divison: " + answer);
         
     }
-    
-    //Clears everything! Or it should clear everything when I get it to work.
-    public final static void clearConsole(){
-    try
-    {
-        final String os = System.getProperty("os.name");
-
-        if (os.contains("Windows"))
-        {
-            Runtime.getRuntime().exec("cls");
-        }
-        else
-        {
-            Runtime.getRuntime().exec("clear");
-        }
-    }
-    catch (final Exception e)
-    {
-        
-    }
-}
 }   
