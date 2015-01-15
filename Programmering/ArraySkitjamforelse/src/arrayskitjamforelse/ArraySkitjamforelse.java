4beaf638f60346ad79ff14ffb5b86354c665f606
xFeber25 <weyenriah@gmail.com>
2015-01-15 14:49:01 +0100

    hej

--- Programmering/ArraySkitjamforelse/src/arrayskitjamforelse/ArraySkitjamforelse.java
+++ Programmering/ArraySkitjamforelse/src/arrayskitjamforelse/ArraySkitjamforelse.java
@@ -7,31 +7,32 @@ public class ArraySkitjamforelse {
     public static void main(String[] args) {
         
         String[] fragor = {
-            "fråga1",
-            "fråga2",
-            "fråga3",
+            "Vad står M*A*S*H för?",
+            "Vad heter huvudkaraktären?",
+            "Tooooo much",
         };
         
         String[][] svar = {
-            {"a"},
-            {"b1","b2"},
-            {"c"},
-            {"d", "d2"},
+            {"mobile army surgical hospital"},
+            {"Hawkeye","benjamin franklin hawkeye pierce"},
+            {"Yes"},
         };
         
-        for(int i = 0; i < svar.length; i++){
+        int poang = 0;
+        
+        for(int i = 0; i < fragor.length; i++){
             
             String s = JOptionPane.showInputDialog(fragor[i]);
             
             for(int it = 0; it < svar[i].length; it++){
                 
                 if(svar[i][it].equalsIgnoreCase(s)){
-                    
-                System.out.println("YAY");
+                    poang++;
+                }
             }
-            }
-            
         }
+        System.out.println(poang + " rätt utav " + fragor.length + " möjliga.");
+        
     }
     
 }
